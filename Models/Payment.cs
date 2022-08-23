60dbe2d53ec6b5cbfe59535b6384e1a46d090d4d
brenobarroso <barrososbreno@icloud.com>
2022-08-23 13:14:37 -0300

    create models

---
+++ Models/Payment.cs
@@ -0,0 +1,18 @@
+using System;
+namespace PaymentAPI.Models
+{
+    public class Payment
+    {
+        
+        public int Id { get; set; } // Identificador NSU
+        public DateTime TransationDate { get; set; } // Data da transação
+        public DateTime ApprovalDate { get; set; } // Data da aprovação (caso ocorra)
+        public DateTime DisapprovalDate { get; set; } // Data de reprovação (caso ocorra)
+        public bool Confirmation { get; set; } // Confirmação da adquirente
+        public float GrossValue { get; set; } // Valor bruto da transação
+        public float NetValue { get; set; } // Valor líquido da transação (descontado taxa)
+        public float FlatRate { get; set; } // Taxa fixa cobrada
+        public String CardNumber { get; set; } // 4 ultimos digitos do cartão
+
+    }
+}
