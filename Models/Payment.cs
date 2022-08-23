14737775bcbfe1288dabfa063b6ffcb593453c67
brenobarroso <barrososbreno@icloud.com>
2022-08-23 15:03:07 -0300

    update

--- Models/Payment.cs
+++ Models/Payment.cs
@@ -1,17 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace PaymentAPI.Models
 {
     public class Payment
     {
-        
+        [Required]
         public int Id { get; set; } // Identificador NSU
+        [Required]
         public DateTime TransationDate { get; set; } // Data da transação
-        public DateTime ApprovalDate { get; set; } // Data da aprovação (caso ocorra)
-        public DateTime DisapprovalDate { get; set; } // Data de reprovação (caso ocorra)
+        public DateTime? ApprovalDate { get; set; } // Data da aprovação (caso ocorra)
+        public DateTime? DisapprovalDate { get; set; } // Data de reprovação (caso ocorra)
         public bool Confirmation { get; set; } // Confirmação da adquirente
         public float GrossValue { get; set; } // Valor bruto da transação
         public float NetValue { get; set; } // Valor líquido da transação (descontado taxa)
+        [Required]
         public float FlatRate { get; set; } // Taxa fixa cobrada
+        [Required]
         public String CardNumber { get; set; } // 4 ultimos digitos do cartão
 
     }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<PaymentDbContext>(
+    o => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
