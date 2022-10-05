53bc0bf357fe91e8fb5c4cc53746eb61866497be
brenobarroso <barrososbreno@icloud.com>
2022-10-05 14:24:58 -0300

    ShouldBeResgistredAnAccount test

--- test/PaymentApiTest/Controllers/AccountControllerTest.cs
+++ test/PaymentApiTest/Controllers/AccountControllerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -27,7 +28,8 @@ public class AccountControllerTest
     public async Task ShouldListAll()
     {
         // Arrange
-        var newAccount1 = new Account{
+        var newAccount1 = new Account
+        {
             CPF = "12345678901",
             Balance = 5000f,
             HolderName = "Breno Santos Barroso",
@@ -35,7 +37,8 @@ public class AccountControllerTest
             IsActive = true
         };
 
-        var newAccount2 = new Account{
+        var newAccount2 = new Account
+        {
             CPF = "12375678901",
             Balance = 5000f,
             HolderName = "Breno Santos",
@@ -43,7 +46,8 @@ public class AccountControllerTest
             IsActive = true
         };
 
-        var newAccount3 = new Account{
+        var newAccount3 = new Account
+        {
             CPF = "12315678901",
             Balance = 5000f,
             HolderName = "Breno Barroso",
@@ -74,7 +78,8 @@ public class AccountControllerTest
     {
         // Arrange
         var id = new Random().Next();
-        var newAccount1 = new Account{
+        var newAccount1 = new Account
+        {
             CPF = "12345678901",
             Balance = 5000f,
             HolderName = "Breno Santos Barroso",
@@ -91,6 +96,92 @@ public class AccountControllerTest
         var result = (OkObjectResult)await accountController.GetByIdAccount(id);
 
         // Assert
+        Assert.Equal(200, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task ShouldNotListAnNotExistingAccountByAccountNumber()
+    {
+        // Arrange
+        var id = new Random().Next();
+
+        var manager = new Mock<IAccountManager>();
+        manager.Setup(x => x.getByAccountNumberAsync(id)).ReturnsAsync((Account)null);
+
+        var accountController = new AccountController(manager.Object);
+
+        // Act
+        var result = (NotFoundResult)await accountController.GetByIdAccount(id);
+
+        // Assert
+        Assert.Equal(404, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task ShouldListAnExistingAccountByCPF()
+    {
+        // Arrange
+
+        var newAccount1 = new Account
+        {
+            CPF = "12345678901",
+            Balance = 5000f,
+            HolderName = "Breno Santos Barroso",
+            Agency = "00239-9",
+            IsActive = true
+        };
+
+        var manager = new Mock<IAccountManager>();
+        manager.Setup(x => x.getByCPFAsync(newAccount1.CPF)).ReturnsAsync(newAccount1);
+
+        var accountController = new AccountController(manager.Object);
+
+        // Act
+        var result = (OkObjectResult)await accountController.GetByCPF(newAccount1.CPF);
+
+        // Assert
+        Assert.Equal(200, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task ShouldNotListAnNotExistingAccountByCPF()
+    {
+        // Arrange
+
+        var manager = new Mock<IAccountManager>();
+        manager.Setup(x => x.getByCPFAsync("15423458741")).ReturnsAsync((Account)null);
+
+        var accountController = new AccountController(manager.Object);
+
+        // Act
+        var result = (NotFoundResult)await accountController.GetByCPF("15423458741");
+
+        // Assert
+        Assert.Equal(404, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task ShouldBeResgistredAnAccount()
+    {
+        // Arrange
+        var newAccount1 = new Account
+        {
+            CPF = "12345678901",
+            Balance = 5000f,
+            HolderName = "Breno Santos Barroso",
+            Agency = "00239-9",
+            IsActive = true
+        };
+        var manager = new Mock<IAccountManager>();
+        manager.Setup(x => x.CreateAccount(newAccount1)).ReturnsAsync(newAccount1);
+
+        var accountController = new AccountController(manager.Object);
+
+        // Act
+        var result = (OkObjectResult)await accountController.Register(newAccount1);
+
+        // Assert
+
         Assert.Equal(200, result.StatusCode);
     }
 }
