59b8750049a9a0d9ddda0adf4e189d4e866d765f
brenobarroso <barrososbreno@icloud.com>
2022-10-24 14:55:06 -0300

    WithdrawViewModel test

--- api/Models/Withdraws/WithdrawViewModel.cs
+++ api/Models/Withdraws/WithdrawViewModel.cs
@@ -1,11 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+using api.Validations.Withdraw;
 
 namespace api.Models.Withdraw;
 
 public class WithdrawViewModel
 {
     [Required]
+    [WithdrawPattern]
     public string AccountNumber { get; set; }
+
     [Required]
+    [WithdrawValue]
     public decimal Value { get; set; }
 }
